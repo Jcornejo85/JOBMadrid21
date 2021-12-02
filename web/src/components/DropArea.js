8bb2919988fb9c639a9ae121fd118cca1ab7631e
Luca Citta <lucacitta@gmail.com>
2021-12-02 16:59:35 +0100

    'algo'

--- web/src/components/DropArea.js
+++ web/src/components/DropArea.js
@@ -2,18 +2,18 @@ import React, { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
 import { useState } from 'react';
 import axios from 'axios';
-import dataFake from '../data/data.json';
+import dataFake from '../data/predict.json';
 
 function MyDropzone(props) {
   const [data, setData] = useState();
-  const [data2, setData2] = useState([dataFake]);
+  const [data2, setData2] = useState(dataFake);
 
   const onDrop = useCallback((acceptedFiles) => {
     setData(acceptedFiles);
     const formData = new FormData();
     formData.append('jsondataRequest', JSON.stringify(acceptedFiles)); //JSON
     console.log('Esto es data2', data2);
-    axios.post(`https://localhost:8000/emc/`, { data2 }).then((res) => {
+    axios.post(`http://localhost:8000/emc/`, { data2 }).then((res) => {
       console.log(res);
       console.log(res.data);
       props.setdataTrue(res.data);
