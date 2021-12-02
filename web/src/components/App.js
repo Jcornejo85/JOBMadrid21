a0baeab8b3465c882222118c1bb0b1be2f99d780
IreneBioque <irenebioque@gmail.com>
2021-12-02 16:38:22 +0100

    add basic style

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -4,10 +4,17 @@ import '../styles/App.scss';
 import MyDropzone from './DropArea';
 
 const App = () => {
+  const [dataTrue, setdataTrue] = useState();
   return (
-    <MyDropzone/>
-
-  )
+    <div className='main'>
+      <h1 className='main__title'>Grupo Adidas</h1>
+      <div className='main__center'>
+        <MyDropzone setdataTrue={setdataTrue} />
+        <button className='main__center--button'>Calcular</button>
+      </div>
+      <div>{props.dataTrue}</div>
+    </div>
+  );
 };
 
 export default App;
--- web/src/components/DropArea.js
+++ web/src/components/DropArea.js
@@ -3,24 +3,23 @@ import { useDropzone } from 'react-dropzone';
 import { useState } from 'react';
 import axios from 'axios';
 import dataFake from '../data/data.json';
-function MyDropzone() {
+
+function MyDropzone(props) {
   const [data, setData] = useState();
   const [data2, setData2] = useState([dataFake]);
-  const [dataTrue, setdataTrue] = useState();
+
   const onDrop = useCallback((acceptedFiles) => {
     setData(acceptedFiles);
     const formData = new FormData();
     formData.append('jsondataRequest', JSON.stringify(acceptedFiles)); //JSON
     console.log('Esto es data2', data2);
-    data2.map((data) => setdataTrue(data));
-    console.log(dataTrue);
-    axios.post(`https://localhost:8000/emc/`, { dataTrue }).then((res) => {
+    axios.post(`https://localhost:8000/emc/`, { data2 }).then((res) => {
       console.log(res);
       console.log(res.data);
+      props.setdataTrue(res.data);
     });
   });
 
-  const handleFetch = () => {};
   const {
     getRootProps,
     getInputProps,
@@ -31,14 +30,13 @@ function MyDropzone() {
     onDrop,
   });
   return (
-    <div {...getRootProps()}>
+    <div {...getRootProps()} className='drop'>
       <input {...getInputProps()} />
       {isDragActive ? (
-        <p>Drop the files here ...</p>
+        <p className='drop__p'>Drop the files here ...</p>
       ) : (
         <p>Drag 'n' drop some files here, or click to select files</p>
       )}
-      <button onClick={handleFetch}>Boton</button>
     </div>
   );
 }
